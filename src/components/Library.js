86af998c8c8c3f054f78d5ab69cf410c0368fecf
Your Name <leobarrientos02@gmail.com>
2021-08-28 20:16:40 -0400

    errors
    Fixing some erros with displaying some of the music data to screen.

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,16 +1,16 @@
 import React from "react";
-
 //Import library song
 import LibrarySong from "./LibrarySong";
 
-const Library = ({songs}) =>{
+const Library = ({ songs }) =>{
     return(
         <div className="library">
             <h2>Library</h2>
             <div className="library-songs">
-               {songs.map( song =>{
-                   <LibrarySong song={song} />
-               })}
+               {songs.map( (song) => (
+                   < LibrarySong song={song} />
+               ))}
+
             </div>
         </div>
     );
